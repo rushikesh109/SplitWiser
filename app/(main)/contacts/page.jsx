f128cfcdcf337598bd2fd01bcdf86c7030a227bb
Rushikesh Mangrule <you@example.com>
2025-07-07 14:30:42 +0530

    contact backendLogic

--- app/(main)/contacts/page.jsx
+++ app/(main)/contacts/page.jsx
@@ -1,9 +1,13 @@
 "use client";
 
+import { api } from '@/convex/_generated/api';
+import { useQuery } from 'convex/react';
 import React from 'react'
 
 const ContactPage = () => {
-  return (
+ const data = useQuery(api.contacts.getAllContacts);
+ console.log(data);
+    return (
     <div>
       ContactPage
     </div>
--- convex/users.js
+++ convex/users.js
@@ -8,27 +8,27 @@ export const store = mutation({
       throw new Error("Called storeUser without authentication present");
     }
 
-    // Check if we've already stored this identity before.
-    // Note: If you don't want to define an index right away, you can use
-    // ctx.db.query("users")
-    //  .filter(q => q.eq(q.field("tokenIdentifier"), identity.tokenIdentifier))
-    //  .unique();
-    const user = await ctx.db
+    const fullName =
+      identity.name ||
+      `${identity.firstName || ""} ${identity.lastName || ""}`.trim() ||
+      "Anonymous";
+
+    const existingUser = await ctx.db
       .query("users")
       .withIndex("by_token", (q) =>
-        q.eq("tokenIdentifier", identity.tokenIdentifier),
+        q.eq("tokenIdentifier", identity.tokenIdentifier)
       )
       .unique();
-    if (user !== null) {
-      // If we've seen this identity before but the name has changed, patch the value.
-      if (user.name !== identity.name) {
-        await ctx.db.patch(user._id, { name: identity.name });
+
+    if (existingUser) {
+      if (existingUser.name !== fullName) {
+        await ctx.db.patch(existingUser._id, { name: fullName });
       }
-      return user._id;
+      return existingUser._id;
     }
-    // If it's a new identity, create a new `User`.
+
     return await ctx.db.insert("users", {
-      name: identity.name ?? "Anonymous",
+      name: fullName,
       tokenIdentifier: identity.tokenIdentifier,
       email: identity.email,
       imageUrl: identity.pictureUrl,
@@ -39,19 +39,21 @@ export const store = mutation({
 export const getCurrentUser = query({
   handler: async (ctx) => {
     const identity = await ctx.auth.getUserIdentity();
-    if(!identity) {
+    if (!identity) {
       throw new Error("Not authenticated");
     }
 
-    const user = await ctx.db.query("users").withIndex("by_token", (q) => {
-      q.eq("tokenIdentifier", identity.tokenIdentifier);
-    })
-    .first();
+    const user = await ctx.db
+      .query("users")
+      .withIndex("by_token", (q) =>
+        q.eq("tokenIdentifier", identity.tokenIdentifier)
+      )
+      .first();
 
-    if(!user){
-      throw new Error ("User not Found");
+    if (!user) {
+      throw new Error("User not found");
     }
 
     return user;
-  }
-})
+  },
+});
